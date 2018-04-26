be5ab9468b4cca7c3a77cb2559360d4a3cd3d579
Matthew Richards <matthew.richards@stfc.ac.uk>
2018-04-26 14:46:15 +0100

    Cleanup existing code
    Cleanup of code from previous commit:
    - Sections of code moved around to suit where they're needed (not used until a certain point)
    - General code improvement, little things

--- AppendSpreadsheet.py
+++ AppendSpreadsheet.py
@@ -5,16 +5,12 @@ from xlutils.copy import copy
 with open("Results.tsv") as tsv, open("Callouts.xls"):
     print('Both files found')
 
-    # Skips first row with column headers
-    iterResults = iter(csv.reader(tsv, dialect="excel-tab"))
-    next(iterResults)
-
     wb = open_workbook("Callouts.xls", formatting_info=True)
-    # Make a writeable copy - not needed at this point
+    # Make a writeable copy
     calloutsBook = copy(wb)
     currentSheet = calloutsBook.get_sheet('Callouts 2018')
 
-    # Find which row to start appending
+    # Find which row to start appending spreadsheet
     firstRow = currentSheet.row(0)
     readSheet = wb.sheet_by_name('Callouts 2018')
     alarmColumn = readSheet.col_values(0)
@@ -26,21 +22,28 @@ with open("Results.tsv") as tsv, open("Callouts.xls"):
         if readSheet.col(0)[i].value == '':
             startAppending = True
             startingRowNumber = i + 1
-        i = i + 1
+        i += 1
 
-    print('Will append spreadsheet from row #' + startingRowNumber - 1)
+    print('Will append spreadsheet from row #' + str(startingRowNumber - 1))
 
+    # Skips first row of TSV file due to column headers
+    iterResults = iter(csv.reader(tsv, dialect="excel-tab"))
+    next(iterResults)
+
+    # Extracting data from TSV file
     for row in iterResults:
         # Getting data from the TSV file
         ticketID = row[0]
         ticketCreated = row[15]
-        if row[20] == '':    # If no Nagios alarm, use ticket's subject - problem when ceph-mon1-5
+        if row[20] == '':    # If no Nagios alarm, use ticket's subject - problem when ceph-mon1-5 callout
             alarm = row[2]
         else:
             alarm = row[20]
 
+    # Putting data into spreadsheet
+
     calloutsBook.save("Callouts.xls")
-    print(currentSheet.name)
+    print('Spreadsheet changes saved')
 
 
 
