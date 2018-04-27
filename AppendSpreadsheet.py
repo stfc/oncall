bbfee5326c1ba4428a7e27809cbde6318c2d5858
Matthew Richards <matthew.richards@stfc.ac.uk>
2018-04-27 15:41:04 +0100

    Separate time and date ticket got created
    - Separates them for the two cells in the spreadsheet
    - The 'outside work hours?' formatting still not working - if time created is doubled clicked in Excel then it works but that's manual

--- AppendSpreadsheet.py
+++ AppendSpreadsheet.py
@@ -1,5 +1,6 @@
 import csv
 from openpyxl import load_workbook
+from datetime import datetime
 
 with open("Results.tsv") as tsv:
     print('Both files found')
@@ -29,7 +30,11 @@ with open("Results.tsv") as tsv:
     i = 0
     for row in iterResults:
         ticketID = row[0]
-        ticketCreated = row[15]
+
+        ticketCreated = datetime.strptime(row[15], '%Y-%m-%d %H:%M:%S')
+        dateCreated = ticketCreated.strftime('%d/%m/%Y')
+        timeCreated = ticketCreated.strftime('%H:%M:%S')
+
         if row[20] == '':    # If no Nagios alarm, use ticket's subject - problem when ceph-mon1-5 callout
             alarm = row[2]
         else:
@@ -37,10 +42,10 @@ with open("Results.tsv") as tsv:
 
         # Putting data into spreadsheet
         currentRow = startingRowNumber + i
-        currentSheet.cell(row=currentRow, column=1).value = alarm    # Alarm name
-        currentSheet.cell(row=currentRow, column=3).value = ticketCreated    # Date issued
-        currentSheet.cell(row=currentRow, column=4).value = ticketCreated    # Time issued
-        currentSheet.cell(row=currentRow, column=5).value = ticketID    # RT query
+        currentSheet.cell(row=currentRow, column=1, value=alarm)          # Alarm name
+        currentSheet.cell(row=currentRow, column=3, value=dateCreated)    # Date issued
+        currentSheet.cell(row=currentRow, column=4, value=timeCreated)    # Time issued
+        currentSheet.cell(row=currentRow, column=5, value=ticketID)       # RT query
         i += 1
 
     wb.save("Callouts.xlsx")
